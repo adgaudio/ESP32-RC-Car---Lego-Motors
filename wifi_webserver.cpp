f2e134adea44596e17dd3d76e71c50560eafa9b6
Alex Gaudio <adgaudio@gmail.com>
2025-04-20 14:20:55 -0400

    js code

--- wifi_webserver.cpp
+++ wifi_webserver.cpp
@@ -5,6 +5,7 @@
 #include <WiFiClientSecure.h> // For HTTPS
 
 
+
 #ifndef SSID_NAME
 #define SSID_NAME "Esp32Car"
 #endif
@@ -15,7 +16,7 @@ WiFiServerSecure server(443); // HTTPS uses port 443
 
 // HTML content
 const char *htmlPage = R"rawliteral(
-<!DOCTYPE html>
+<!doctype html>
 <html>
   <head>
     <title>ESP32 Car Control</title>
@@ -25,15 +26,13 @@ const char *htmlPage = R"rawliteral(
     <p>Use your phone's tilt to control the car!</p>
     <p id="payload_viewer"></p>
     <button id="direction_button" type="button">Reverse Direction</button>
-  </body>
+    <button id="permission_button" type="button">Enable Motion Control</button>
 
     <script>
-
-
 // Function to convert JSON to URL-encoded string
 function encodeParams(data) {
   return Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
     .join("&");
 }
 
@@ -41,71 +40,78 @@ function encodeParams(data) {
 let can_send = true;
 function sendpayload(payload) {
   can_send = false;
-  fetch('/control_motors', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: encodeParams(payload)
-  }).then(response => {
-  can_send = true;
-// document.getElementById("paHyload_viewer").innerHTML = response.text();
-    });
-  console.log(payload);
-  document.getElementById("payload_viewer").innerHTML = `<pre> ${JSON.stringify(payload,null,2)} </pre>`;
-}
-
-if (typeof DeviceMotionEvent.requestPermission === 'function') {
-    DeviceMotionEvent.requestPermission()
-        .then(permissionState => {
-            if (permissionState === 'granted') {
-                window.addEventListener('devicemotion', handleMotionEvent);
-            } else {
-                console.warn("Motion access denied");
-            }
-        })
-        .catch(console.error);
-} else {
-    // handle regular non iOS 13+ devices
-    window.addEventListener('deviceorientation', handleMotionEvent);
+  fetch("/control_motors", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: encodeParams(payload),
+  }).then((response) => {
+    can_send = true;
+  });
 }
 
 let direction = 0;
 function handleMotionEvent(event) {
-      // direction:  get relative speed of left and right motor when turning L or R
-      let g = event.gamma; // Left/right tilt
-      if (g < -30) { g = -30; }
-      else if (g > 30) { g = 30; }
-      let w_r, w_l;  // direction weights, in [0,1]
-      if (g > 0) {
-        w_r = 1 - g/30;
-        w_l = 1;
-      } else {
-        w_l = 1 - g/-30;
-        w_r = 1;
-      }
-      // speed: get overall throttle value, using [-45,0] degrees as stop,max_speed limits
-      let b = event.beta; // Forward/backward tilt
-      if (b < 0) { b = 0; }
-      else if (b > 45) { b = 45; }
-      w_speed = b / 45;
+  let g = event.gamma; // Left/right tilt
+  g = Math.max(-30, Math.min(30, g));
+  let w_r = g > 0 ? 1 - g / 30 : 1;
+  let w_l = g > 0 ? 1 : 1 - g / -30;
 
-  //     sendpayload({
-  //     gamma: event.gamma, beta: event.beta,
-  //   w_speed, w_l, w_r, direction
-  // });
+  let b = event.beta; // Forward/backward tilt
+  b = Math.max(0, Math.min(45, b));
+  let w_speed = b / 45;
+
+  const payload = {
+    left_direction: direction,
+    right_direction: direction,
+    left_speed: Math.round(255 * w_l * w_speed),
+    right_speed: Math.round(255 * w_r * w_speed),
+  };
+  console.log(payload);
+  document.getElementById("payload_viewer").innerHTML =
+    `<pre>${JSON.stringify(payload, null, 2)}  ${event.beta}  ${event.gamma}</pre>`;
   if (can_send) {
-      sendpayload({
-        left_direction: direction,
-        right_direction: direction,
-        left_speed: parseInt(255 * w_l * w_speed),
-        right_speed: parseInt(255 * w_r * w_speed),
-      });
+    sendpayload(payload);
   }
-  }
+}
 
-document.getElementById("direction_button").addEventListener("click", () => {
-  direction = 1-direction;
+// Add click event to request motion access on iOS
+
+function requestPermissions() {
+  if (typeof DeviceMotionEvent.requestPermission === "function") {
+    DeviceMotionEvent.requestPermission()
+      .then((permissionState) => {
+        if (permissionState === "granted") {
+          document.getElementById("permission_button").disabled = true;
+          window.addEventListener("devicemotion", handleMotionEvent);
+        } else {
+          console.warn("Motion access denied");
+        }
+      })
+      .catch(console.error);
+  } else {
+    document.getElementById("permission_button").disabled = true;
+    console.warn("device does not have DeviceMotionEvent API not available. Trying to fall back to a deprecated approach.");
+    document.getElementById("permission_button").innerHTML = "DeviceMotionEvent API not available"
+    // handle regular non iOS 13+ devices
+    window.addEventListener("deviceorientation", handleMotionEvent);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  // android
+  requestPermissions();
+  // ios
+  document
+    .getElementById("permission_button")
+    .addEventListener("click", requestPermissions);
+
+  document.getElementById("direction_button").addEventListener("click", () => {
+    direction = 1 - direction;
+  });
 });
+
     </script>
+  </body>
 </html>
 )rawliteral";
 
